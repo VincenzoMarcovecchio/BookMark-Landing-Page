480e5744604ddc2b9e3644bc01989c7848f06304
VIncenzo Marcovecchio <vinceoldmark@gmail.com>
2020-01-13 17:30:14 +0100

    we need to make the button go down when error is on

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ $("#toggle").change(function() {
   } else {
     $("section").css("zIndex", 0);
     $(".nav-logo").css({
-      backgroundImage: "url(/images/logo-bookmark.svg)",
+      backgroundImage: "url(./images/logo-bookmark.svg)",
       zIndex: "inherit",
       position: "inherit"
     });
@@ -36,3 +36,52 @@ tabs.forEach(tab => {
     tab.classList.add("active");
   });
 });
+
+const myForm = document.getElementById("myForm");
+const email = document.getElementById("email");
+
+myForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function setErrorFor(input) {
+  const formControl = document.getElementById("error");
+  formControl.style.display = "inherit";
+}
+
+function checkInputs(input) {
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    setErrorFor(email);
+    changeEmail();
+    marginButton();
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email);
+    changeEmail();
+    marginButton();
+  } else {
+    setSuccessFor(email);
+  }
+}
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
+
+function changeEmail() {
+  const placeholder = document.getElementById("email");
+  placeholder.placeholder = "email@example.com";
+}
+
+function marginButton() {
+  const button = document.getElementById("button");
+  if (window.innerWidth <= 750) {
+    button.style.marginTop = "20px";
+  } else if (window.innerWidth != 750) {
+    button.style.marginTop = 0;
+  }
+}
