a84ba0353d427ca89f2b48cd8329ff9d836ad8ed
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2020-05-04 13:47:55 +0000

    too poor for this

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
-$(document).ready(function() {
-  $("#first").addClass("active");
+$(document).ready(function () {
+  $('#first').addClass('active');
 });
 
-$("#toggle").change(function() {
+$('#toggle').change(function () {
   if (this.checked) {
-    $("section").css("zIndex", -3000);
-    $(".nav-logo").css({
-      backgroundImage: "url(./images/logo-bookmark-mobile.svg)",
-      zIndex: "50",
-      position: "fixed"
+    $('section').css('zIndex', -3000);
+    $('.nav-logo').css({
+      backgroundImage: 'url(./images/logo-bookmark-mobile.svg)',
+      zIndex: '50',
+      position: 'fixed',
     });
   } else {
-    $("section").css("zIndex", 0);
-    $(".nav-logo").css({
-      backgroundImage: "url(./images/logo-bookmark.svg)",
-      zIndex: "inherit",
-      position: "inherit"
+    $('section').css('zIndex', 0);
+    $('.nav-logo').css({
+      backgroundImage: 'url(./images/logo-bookmark.svg)',
+      zIndex: 'inherit',
+      position: 'inherit',
     });
   }
 });
 
-const tabs = document.querySelectorAll("[data-tab-target]");
-const tabContents = document.querySelectorAll("[data-tab-content]");
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
 
-tabs.forEach(tab => {
-  tab.addEventListener("click", () => {
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget);
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove("active");
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active');
     });
-    tabs.forEach(tab => {
-      tab.classList.remove("active");
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
     });
-    target.classList.add("active");
-    tab.classList.add("active");
+    target.classList.add('active');
+    tab.classList.add('active');
   });
 });
 
-const myForm = document.getElementById("myForm");
-const email = document.getElementById("email");
+const myForm = document.getElementById('myForm');
+const email = document.getElementById('email');
 
-myForm.addEventListener("submit", e => {
-  e.preventDefault();
-
+myForm.addEventListener('submit', (e) => {
   checkInputs();
+  e.preventDefault();
 });
 
 function setErrorFor(input) {
-  const formControl = document.getElementById("error");
-  formControl.style.display = "inherit";
-  document.querySelector(".image-error").style.display = "inherit";
+  const error = document.getElementById('error');
+  error.style.display = 'inherit';
+  document.querySelector('.image-error').style.display = 'inherit';
 }
 
 function checkInputs(input) {
   const emailValue = email.value.trim();
 
-  if (emailValue === "") {
+  if (emailValue === '') {
     setErrorFor(email);
-    changeEmail();
+    changeEmail(email);
     marginButton();
   } else if (!isEmail(emailValue)) {
     setErrorFor(email);
@@ -74,15 +73,27 @@ function isEmail(email) {
 }
 
 function changeEmail() {
-  const placeholder = document.getElementById("email");
-  placeholder.placeholder = "email@example.com";
+  const placeholder = document.getElementById('email');
+  placeholder.placeholder = 'email@example.com';
 }
 
+const buttone = document.querySelectorAll('Bottone');
+
 function marginButton() {
-  const button = document.getElementById("button");
+  const button = document.getElementById('button');
+
+  if (window.innerWidth <= 750 && setErrorFor()) {
+    button.style.marginTop = '20px';
+  }
   if (window.innerWidth <= 750) {
-    button.style.marginTop = "20px";
+    button.style.marginTop = '20px';
   } else if (window.innerWidth >= 750) {
-    button.style.marginTop = "0px";
+    button.style.marginTop = '0px';
   }
 }
+
+buttone.forEach((bu) => {
+  bu.addEventListener('click', () => {
+    bu.classList.toggle('arrow-up');
+  });
+});
